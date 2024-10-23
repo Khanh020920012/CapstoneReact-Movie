f9c88485006e2a6dcd19a6763a7be59073d885df
VSOP40 <phongnguyenwm3@gmail.com>
2024-10-23 13:21:13 +0700

    update baseURL

--- src/services/config.js
+++ src/services/config.js
@@ -4,7 +4,7 @@ import { startLoading, stopLoading } from "../redux/slice/loadingSlice";
 import { store } from "../redux/configStore";
 export const http = axios.create({
   // Your axios configurations
-  baseURL: "https://movienew.cybersoft.edu.vn/api",
+  baseURL: "https://movienew.cybersoft.edu.vn/swagger/api",
   headers: {
     tokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAxMCIsIkhldEhhblN0cmluZyI6IjAxLzA5LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcyNTE0ODgwMDAwMCIsIm5iZiI6MTY5ODY4NTIwMCwiZXhwIjoxNzI1Mjk2NDAwfQ.CPY1b9IiMcklQZ9hjqIzrdiOlQ5YnV4VpzGu_yZr7G0",
